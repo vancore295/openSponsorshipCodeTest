084deec08f08e7af18d5a9018b2086524ecb1407
vancore295 <silverfire295@gmail.com>
2017-09-19 07:38:56 -0400

    update api call

--- controllers/athleteController.js
+++ controllers/athleteController.js
@@ -11,23 +11,7 @@
 
         app.put("/api/addNewAthlete", function(req, res) {
             console.log(req.body);
-            var newAthlete = new athlete({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                date_of_birth: req.body.date_of_birth,
-                nationality: req.body.nationality,
-                location: req.body.location,
-                Association: req.body.Association,
-                team: req.body.team,
-                Sports: req.body.Sports,
-                about: req.body.about,
-                Interests: req.body.Interests,
-                Charities: req.body.Charities,
-                Social_Media_Handles: req.body.Social_Media_Handles,
-                Pets: req.body.Pets,
-                Drinks_Alcohol: req.body.Drinks_Alcohol,
-                Married: req.body.Married
-            });
+            var newAthlete = new athlete(req.body);
             newAthlete.save();
         });
     }
